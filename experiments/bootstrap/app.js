a5f0a4c8bc8706f588d30d16df37324febbf4f90
Jamie Muir <jaamiem@users.noreply.github.com>
2018-03-26 05:02:31 +0100

    bootstrap nav improvements

--- experiments/bootstrap/app.js
+++ experiments/bootstrap/app.js
@@ -4,6 +4,33 @@ var ejs = require('ejs');
 
 var app = express();
 
+const spots = [
+	{
+		id: 1,
+		name: "College Street Car Park",
+		lat: 57.142736,
+		lng: -2.099031
+	},
+	{
+		id: 2,
+		name: "Crown Terrace",
+		lat: 57.144050,
+		lng:-2.101225
+	},
+	{
+		id: 3,
+		name: "College Street Car Park",
+		lat: 57.142736,
+		lng: -2.099031
+	},
+	{
+		id: 4,
+		name: "Crown Terrace",
+		lat: 57.144050,
+		lng:-2.101225
+	}
+]
+
 //  Set the view engine to read EJS files for templating
 app.set('view engine', 'ejs');
 
@@ -11,15 +38,17 @@ app.use(express.static(__dirname + '/public'));
 
 //  Initial page
 app.get('/', (req, res) => {
-	res.render('home');
+	res.render('home', { title: 'Home', spots: spots });
 });
 
+//  About Page
 app.get('/about', (req,res) => {
-	res.render('about');
+	res.render('about', { title: 'About' });
 })
 
+//  Help Page
 app.get('/help', (req,res) => {
-	res.render('help');
+	res.render('help', { title: 'Help' });
 })
 
 app.listen(8080);
