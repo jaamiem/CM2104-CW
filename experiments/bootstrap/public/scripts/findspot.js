7f3d0e61f6521af309572e4fada9a7e1ef9d5901
rgu1602399 <1602399@rgu.ac.uk>
2018-04-09 17:10:18 +0100

    добавил материал из лекции

--- experiments/bootstrap/public/scripts/findspot.js
+++ experiments/bootstrap/public/scripts/findspot.js
@@ -1,14 +1,17 @@
 $(function() {
+	$('#closeSearch').click(function() {
+		$('#resultsPane').css('display', 'none');
+	});
+	
+	$('#login').css('display', 'none');
+	$('#buttonContainer').append("<a href='#' id='loginButton' class='btn accBtn active'>Login</a>");
+	
 	$('#loginButton').click(function() {
 		if ($('#login').css('display') == 'none') {
 			$('#login').css('display', 'block');
 		} else {
 			$('#login').css('display', 'none');
 		}
-	});
-	
-	$('#closeSearch').click(function() {
-		$('#resultsPane').css('display', 'none');
 	});
 });
 
