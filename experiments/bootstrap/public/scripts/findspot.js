1c5f7fc8683563e5137e3254148d17bbc8215575
rgu1602399 <1602399@rgu.ac.uk>
2018-04-14 13:00:19 +0100

    burger now edible. still inadequate.

--- experiments/bootstrap/public/scripts/findspot.js
+++ experiments/bootstrap/public/scripts/findspot.js
@@ -6,13 +6,21 @@ $(function() {
 	// Add elements to the menu (these are not displayed with JS disabled)
 	$('#login').css('display', 'none');
 	$('#loginButtonContainer').append("<a href='#' id='loginButton' class='btn accBtn active'>Login</a>");
-	$('#burgerContainer').append("<a href='#' class='btn accBtn active'>&equiv;</a>");
+	$('#burgerContainer').append("<a href='#' id='burgerButton' class='btn accBtn active'>&equiv;</a>");
 	
 	$('#loginButton').click(function() {
 		if ($('#login').css('display') == 'none') {
 			$('#login').css('display', 'block');
 		} else {
 			$('#login').css('display', 'none');
+		}
+	});
+	
+	$('#burgerButton').click(function() {
+		if ($('.linkbar').css('display') == 'none') {
+			$('.linkbar').css('display', 'flex');
+		} else {
+			$('.linkbar').css('display', 'none');
 		}
 	});
 });
