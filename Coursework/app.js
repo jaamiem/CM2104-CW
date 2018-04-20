462e754d8ca1029dfd56e67332f0f10170104e2d
rgu1602399 <1602399@rgu.ac.uk>
2018-04-20 15:56:34 +0100

    Refixed login system and replaced google maps with leaflet.js

--- Coursework/app.js
+++ Coursework/app.js
@@ -107,7 +107,6 @@ app.post('/locations', function(req, res) {
 // the dologin route which takes data from our login page
 // post variables, username and password
 app.post('/dologin', function(req,res){
-	console.log(JSON.stringfy(req.body))
 	var username = req.body.username;
 	var password = req.body.password;
 	
