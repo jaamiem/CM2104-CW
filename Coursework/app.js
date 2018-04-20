1dbaefa13ce895f5ef4ecdef46639aeccf11b3c6
MMuller16 <1604261@rgu.ac.uk>
2018-04-20 15:34:34 +0100

    Update app.js

--- Coursework/app.js
+++ Coursework/app.js
@@ -109,13 +109,13 @@ app.get('/login', function(req, res){
 //start of Post Routes 
 
 // db stuff for user_inputed_locations 
-app.post('/locations', function(req, res) {
+/*app.post('/locations', function(req, res) {
     db.collection('locations').save(req.body, function(err, result) {
         if (err) throw err;
         console.log('location added to database')
         res.redirect('/')
      })
- })
+ })*/
 
 // the dologin route which takes data from our login page
 // post variables, username and password
