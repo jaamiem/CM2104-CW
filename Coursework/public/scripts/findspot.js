cb7438eaa140d1bf1dadd588156469237eff8132
rgu1602399 <1602399@rgu.ac.uk>
2018-04-27 14:03:56 +0100

    A great philosopher once wrote "Naughty, Naughty, Very naughty"

--- Coursework/public/scripts/findspot.js
+++ Coursework/public/scripts/findspot.js
@@ -57,7 +57,7 @@ $(function() {
  	// Create the search box and link it to the UI element.
         var input = document.getElementById('locationSearch');
         var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 
 	// Listeners for changes in the value of the rating and price sliders.
