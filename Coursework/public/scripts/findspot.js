38247e4b1ded90b27001d70dd08a40c1c437ed92
MMuller16 <1604261@rgu.ac.uk>
2018-04-27 15:06:46 +0100

    callback function

--- Coursework/public/scripts/findspot.js
+++ Coursework/public/scripts/findspot.js
@@ -90,6 +90,15 @@ $(function() {
 		radius: '50',
 		type: ['locations']
 	};
+	
+	function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
 
 	service = new google.maps.places.PlacesService(map);
 	service.nearbySearch(request);
